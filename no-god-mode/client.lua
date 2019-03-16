21020cb0cb6d9994ef442bdbbb25a9ad41499d52
JHodgson1 <hodgsonjack@hotmail.com>
2019-03-16 23:05:47 +0000

    Check every 500 ticks for god mode -- more optimised. Clean up code. Rename vars.

--- no-god-mode/client.lua
+++ no-god-mode/client.lua
@@ -1,28 +1,33 @@
-Citizen.CreateThread(function()
+local isInvincible = false
 
+Citizen.CreateThread(function()
+    while true do
+        isInvincible = GetPlayerInvincible(PlayerId())
+        Citizen.Wait(500)
+    end
+end)
+
+Citizen.CreateThread(function()
     while true do
         
-        local player_l = PlayerPedId()
-        local ped_l = PlayerId()
-        if GetPlayerInvincible(ped_l) then
+        local ped = PlayerPedId()
+        local player = PlayerId()
+        if isInvincible then
             DrawLabel("~r~You are currently in godmode which is ~h~prohibited~h~ on this server, disable it now.")
-            FreezeEntityPosition(player_l, true)
-            DisablePlayerFiring(ped_l, true)
-            if IsPedInAnyVehicle(player_l, false) then
-                FreezeEntityPosition(GetVehiclePedIsIn(player_l, false), true)
+            FreezeEntityPosition(ped, true)
+            DisablePlayerFiring(player, true)
+            if IsPedInAnyVehicle(ped, false) then
+                FreezeEntityPosition(GetVehiclePedIsIn(ped, false), true)
             end
         else
-            FreezeEntityPosition(player_l, false) 
-            DisablePlayerFiring(ped_l, false)      
-            if IsPedInAnyVehicle(player_l, false) then
-                FreezeEntityPosition(GetVehiclePedIsIn(player_l, false), true)
+            FreezeEntityPosition(ped, false)
+            if IsPedInAnyVehicle(ped, false) then
+                FreezeEntityPosition(GetVehiclePedIsIn(ped, false), false)
             end 
         end
         
         Citizen.Wait(0)
     end
-
-
 end)
 
 function DrawLabel(text)
